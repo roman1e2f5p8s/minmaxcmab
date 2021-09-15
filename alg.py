4c5e2b60d1da013cbeb6bdc27d7d5c2ff7ac4712
Roman <roman.1e2f5p8s.over3@gmail.com>
2021-09-15 18:03:35 +0100

    Got rid of noise in H and Y

--- alg.py
+++ alg.py
@@ -1,5 +1,7 @@
 import numpy as np
 from termcolor import colored as tc
+import warnings
+warnings.filterwarnings("error")
 
 
 class Alg:
@@ -7,36 +9,31 @@ class Alg:
         self.feature_dim = feature_dim
         self.n_arms = n_arms
         
-        self.H = np.zeros(shape=(n_arms, feature_dim, feature_dim), dtype=np.float64)   # F.T * F
-        self.Y = np.zeros(shape=(n_arms, feature_dim), dtype=np.float64)                # F.T * r
+        self.H = np.zeros(shape=(n_arms, 1, feature_dim), dtype=np.float64) # F.T * F
+        self.Y = np.zeros(shape=(n_arms, 1), dtype=np.float64)              # F.T * r
 
-        # TODO: in general, C, R, S can be different for each arm
         self.C = 1.00 * np.identity(n=feature_dim, dtype=np.float64)
-        # self.R = 2.0e-1 * np.identity(n=feature_dim, dtype=np.float64)
         self.S = 1e+6 * np.identity(n=feature_dim, dtype=np.float64)
         self.S0 = 1e-2 * np.identity(n=feature_dim, dtype=np.float64)
 
-        # self.R_inv = np.linalg.pinv(self.R)
-        # self.S_inv = np.linalg.pinv(self.S)
-
-        self.R = np.empty_like(self.H)
+        self.R = np.empty(shape=(n_arms, self.H.shape[1]), dtype=np.float64)
         self._idle(features, true_theta)
 
-        self.P = np.empty_like(self.H)
-        self.Theta = np.zeros_like(self.Y)
+        self.P = np.empty(shape=(n_arms, feature_dim, feature_dim), dtype=np.float64)
+        self.Theta = np.zeros(shape=(n_arms, feature_dim), dtype=np.float64)
         self.alpha = np.empty(shape=n_arms, dtype=np.float64)
         self.beta = np.empty_like(self.alpha)
 
         for arm in range(n_arms):
-            self.P[arm] = self.S0 + self.H[arm].T @ self.R[arm] @ self.H[arm]
-            self.Theta[arm] = np.linalg.inv(self.P[arm]).dot(self.H[arm].T).dot(self.R[arm]).dot(self.Y[arm])
+            self.P[arm] = self.S0 + (self.H[arm].T * self.R[arm]) @ self.H[arm]
+            self.Theta[arm] = np.dot(np.linalg.inv(self.P[arm]),
+                    self.H[arm].T * self.R[arm] * self.Y[arm])[:, 0]
             self.alpha[arm] = np.dot(self.R[arm].dot(self.Y[arm]), self.Y[arm])
             self.beta[arm] = 1 - self.alpha[arm] + np.dot(self.P[arm].dot(self.Theta[arm]),
                     self.Theta[arm])
 
             # sanity check
             assert 0 < self.beta[arm] < 1
-            # print('1-beta[{}] = {:.6f}'.format(arm, 1 - self.beta[arm]))
             assert (np.linalg.norm(self.Theta[arm] - true_theta[arm]) < \
                     np.sqrt(self.beta[arm] * (1 / np.linalg.eig(self.P[arm])[0]).max()))
 
@@ -48,17 +45,24 @@ class Alg:
 
     def choose_arm(self, features):
         for arm in range(self.n_arms):
-            self.p[arm] = np.dot(features[arm], self.Theta[arm]) + \
+            try:
+                self.p[arm] = np.dot(features[arm], self.Theta[arm]) + \
                     np.sqrt(self.beta[arm] * np.dot(np.linalg.inv(self.P[arm]).dot(features[arm]),
                         features[arm]))
+            except RuntimeWarning:
+                print(self.beta[arm])
+                print(np.dot(np.linalg.inv(self.P[arm]).dot(features[arm]),
+                        features[arm]))
+                
+                exit()
         self.arm = np.argmax(self.p)
         self.t += 1
 
         return self.arm
 
     def update(self, chosen_arm, chosen_arm_features, reward, true_theta):
-        self.H[chosen_arm] += np.outer(chosen_arm_features, chosen_arm_features)
-        self.Y[chosen_arm] += reward * chosen_arm_features
+        self.H[chosen_arm] = chosen_arm_features
+        self.Y[chosen_arm] = reward
 
         arm = chosen_arm
         B_prev = self.P[arm] + self.C.T @ self.S @ self.C
@@ -67,46 +71,33 @@ class Alg:
 
         self.alpha[arm] = self.alpha[arm] + np.dot(self.R[arm].dot(self.Y[arm]), self.Y[arm]) -\
                 np.dot(P_prev_Theta, np.dot(B_prev_inv, P_prev_Theta))
-        self.P[arm] = self.S + self.H[arm].T @ self.R[arm] @ self.H[arm] -\
+        self.P[arm] = self.S + (self.H[arm].T * self.R[arm]) @ self.H[arm] -\
                 self.S @ self.C @ B_prev_inv @ self.C.T @ self.S
         self.Theta[arm] = np.dot(
                 np.linalg.inv(self.P[arm]), 
                 (np.dot(self.S @ self.C, np.dot(B_prev_inv, P_prev_Theta)) +\
-                        np.dot(self.H[arm].T, self.R[arm].dot(self.Y[arm])))
-            )
+                        (self.H[arm].T * self.R[arm] * self.Y[arm])[:, 0]))
         self.beta[arm] = 1 - self.alpha[arm] + np.dot(self.P[arm].dot(self.Theta[arm]),
                 self.Theta[arm])
 
-        # print(self.beta[arm])
         assert 0 < self.beta[arm] < 1
-        assert (np.linalg.norm(self.Theta[arm] - true_theta[arm]) < \
-                np.sqrt(self.beta[arm] * (1 / np.linalg.eig(self.P[arm])[0]).max()))
-        # self.err = np.sqrt(self.beta[arm] * (1 / np.linalg.eig(self.P[arm])[0]).max())
-        self.err = self.beta
+        # TODO: uncomment the assertion if necessary
+        # assert (np.linalg.norm(self.Theta[arm] - true_theta[arm]) < \
+                # np.sqrt(self.beta[arm] * (1 / np.linalg.eig(self.P[arm])[0]).max()))
+        self.S *= 1.0001
     
-        if False:
-            print('t={:6d} | played arm: {} |'.format(self.t, self.arm), end=' ')
-            for arm in range(self.n_arms):
-                # x = np.linalg.norm(self.Theta[arm])
-                x = np.sqrt(self.beta[arm] * (1 / np.linalg.eig(self.P[arm])[0]).max())
-                s = 'arm={}: {:.4f}'.format(arm, x)
-                s = tc(s, 'green') if arm == self.arm else s
-                print('{} |'.format(s), end=' ')
-            print()
-
     def _idle(self, features, true_theta):
         BEST_ARMS = [3, 7, 9, 15]
         for arm in range(self.n_arms):
-            self.R[arm] = 2.0e-1 * np.identity(n=self.feature_dim, dtype=np.float64)
+            self.R[arm] = 1.0 * np.identity(n=self.R.shape[1], dtype=np.float64)
             if arm in BEST_ARMS:
-                self.R[arm] *= 5
+                self.R[arm] *= 1.2
 
-            self.Y[arm] = (features[arm].dot(true_theta[arm]) + 0.01 * np.random.randn()) * features[arm]
-            self.H[arm] = np.outer(features[arm], features[arm])
+            self.Y[arm] = features[arm].dot(true_theta[arm]) + 0.01 * np.random.randn()
+            self.H[arm] = features[arm]
 
             # sanity check
             x = self.Y[arm] - self.H[arm].dot(true_theta[arm])
             iTheta = np.dot(self.S0.dot(true_theta[arm]), true_theta[arm]) +\
                     np.dot(self.R[arm].dot(x), x)
-            # print('iTheta[{}] = {:.6f}'.format(arm, iTheta))
             assert iTheta <= 1
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ np.random.seed(123)
 
 FEATURE_DIM = 5
 N_ARMS = 16
-N_TRIALS = 10000
+N_TRIALS = 20000
 T = N_TRIALS
 ALPHA = 2.5
 
@@ -33,15 +33,16 @@ def true_theta(n_arms, feature_dim):
     return theta
 
 
-def reward_func(theta, context):
+def reward_func(theta, context, noise):
     signal = np.dot(theta, context)
-    noise = 0.009 * np.random.randn()
+    # noise = 0.01 * np.random.randn()
 
     return signal + noise
 
 
 DATA = np.random.uniform(low=0, high=1, size=(N_TRIALS, N_ARMS, FEATURE_DIM))
 TRUE_THETA = true_theta(N_ARMS, FEATURE_DIM)
+NOISE = 0.01 * np.random.randn(N_TRIALS)
 
 if TEST:
     DATA = dummy_data.DATA
@@ -52,7 +53,7 @@ if TEST:
 
 
 EXPECTED_REWARDS = np.array([
-        np.max([reward_func(TRUE_THETA[arm], DATA[t, arm]) for arm in range(N_ARMS)]) \
+        np.max([reward_func(TRUE_THETA[arm], DATA[t, arm], NOISE[t]) for arm in range(N_ARMS)]) \
         for t in np.arange(N_TRIALS)
         ])
 
@@ -68,21 +69,20 @@ if ALG:
     ub = np.empty((N_TRIALS, N_ARMS))
 
 for t in range(T):
-    print('t={}'.format(t), end='\r')
+    print('t={} of {}'.format(t, N_TRIALS), end='\r')
     features = DATA[t]
     
     if LINUCB:
         chosen_arm = linucb.choose_arm(features)
-        reward = reward_func(TRUE_THETA[chosen_arm], features[chosen_arm])
+        reward = reward_func(TRUE_THETA[chosen_arm], features[chosen_arm], NOISE[t])
         linucb.update(chosen_arm, features[chosen_arm], reward)
         estimated_rewards_l[t] = reward
 
     if ALG:
         chosen_arm = alg.choose_arm(features)
-        reward = reward_func(TRUE_THETA[chosen_arm], features[chosen_arm])
+        reward = reward_func(TRUE_THETA[chosen_arm], features[chosen_arm], NOISE[t])
         alg.update(chosen_arm, features[chosen_arm], reward, TRUE_THETA)
         estimated_rewards_a[t] = reward
-        ub[t] = alg.err
 
 if ALG:
     theta_err = [np.linalg.norm(alg.Theta[arm] - TRUE_THETA[arm]) / np.linalg.norm(TRUE_THETA[arm]) \
@@ -95,8 +95,6 @@ if LINUCB:
 if ALG:
     regret_a = np.cumsum(EXPECTED_REWARDS[:T] - estimated_rewards_a[:T])
     plt.plot(regret_a, label='Alg')
-    # for arm in range(N_ARMS):
-        # plt.plot(ub[:, arm], label=str(arm))
 
 plt.xlabel('Trials')
 plt.ylabel('Regret')
