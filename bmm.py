0c90256ffa308d6c62ed1ad3b29a32b81e3b49db
Roman <roman.1e2f5p8s.over3@gmail.com>
2022-06-16 16:05:11 +0100

    BMM imlemented

---
+++ bmm.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+class Bmm:
+    def __init__(self, feature_dim: int, n_arms: int, eps: float, delta: float, v: float, T: int):
+    # def __init__(self, feature_dim, n_arms, alpha):
+        assert (feature_dim > 0 and type(feature_dim) == int)
+        self.feature_dim = feature_dim  # d in the paper
+
+        assert (n_arms > 0 and type(n_arms) == int)
+        self.n_arms = n_arms  # K in the paper
+
+        assert 0 < eps <= 1
+        self.eps = eps
+        self.v = v
+
+        assert 0 < delta <= 1
+        self.delta = delta
+        self.r = int(np.ceil(8 * np.log(2 * n_arms * T * np.log(T) / delta)))
+        
+        assert (T > 0 and type(T) == int)
+        self.T = T
+
+        self.t = 0
+        self.Psi = np.empty(shape=0, dtype=np.int32)
+
+        self.arms = np.empty(shape=T, dtype=np.int32)
+        
+        # BMM requires storing contexts x for each time t and each arm a 
+        self.x = np.zeros(shape=(T, n_arms, feature_dim), dtype=np.float64)
+
+        self.A = np.identity(n=feature_dim, dtype=np.float64)
+        self.A_inv = np.linalg.inv(self.A)
+
+        self.b = np.zeros(shape=(self.r, feature_dim), dtype=np.float64)
+
+        self.reward = np.zeros(shape=(self.r, T, n_arms), dtype=np.float64)
+
+        self.Theta_hat = np.zeros_like(self.b, dtype=np.float64)
+
+        # oracle
+        self.p = np.empty(shape=n_arms, dtype=np.float64)
+
+        # self.b = np.zeros(shape=(n_arms, feature_dim), dtype=np.float64)
+        # self.A = np.empty(shape=(n_arms, feature_dim, feature_dim), dtype=np.float64)
+        # for arm in range(n_arms):
+            # self.A[arm] = np.identity(n=feature_dim, dtype=np.float64)
+        # self.p = np.empty(shape=n_arms, dtype=np.float64)
+    
+    def _alpha(self):
+        return np.power(12 * self.v, 1 / (1 + self.eps)) * np.power(self.t + 1, 0.5 * (1 - self.eps) /\
+                (1 + self.eps))
+
+    def choose_arm(self, features):
+        # self.t starts from 0
+        for arm in range(self.n_arms):
+            self.x[self.t, arm] = features[arm]
+
+            r_hat = np.median(np.array(
+                    [np.dot(self.x[self.t, arm].T, self.Theta_hat[j]) for j in range(self.r)],
+                    dtype=np.float64,
+                    ))
+
+            w = (1 + self._alpha()) * np.sqrt(
+                    np.dot(
+                        self.x[self.t, arm].T,
+                        np.dot(
+                            self.A_inv,
+                            self.x[self.t, arm]
+                            )
+                        )
+                    )
+            
+            self.p[arm] = r_hat + w
+
+            # A_inv = np.linalg.inv(self.A[arm])
+            # self.p[arm] = np.dot(np.dot(A_inv, self.b[arm]).T, features[arm]) + \
+                    # self.alpha * np.sqrt(np.dot(features[arm].T, np.dot(A_inv, features[arm])))
+
+        self.arms[self.t] = np.argmax(self.p)
+
+        return self.arms[self.t]
+
+    def update(self, rewards):
+        # rewards --- self.r rewards received when playing choose_arm arm self.r times
+        self.A = np.identity(n=self.feature_dim, dtype=np.float64)
+
+        self.reward[:, self.t, self.arms[self.t]] = rewards
+
+        for j in range(self.r):
+            self.b = np.zeros(shape=(self.r, self.feature_dim), dtype=np.float64)
+            for tau in self.Psi:
+                if not j:
+                    self.A += self.x[tau-1, self.arms[tau-1]] @ self.x[tau-1, self.arms[tau-1]].T
+                    # self.A += np.outer(self.x[tau-1, self.arms[tau-1]], self.x[tau-1, self.arms[tau-1]])
+                    # self.A += np.dot(self.x[tau-1, self.arms[tau-1]], self.x[tau-1, self.arms[tau-1]])
+
+                self.b[j] += self.reward[j, tau-1, self.arms[tau-1]] * self.x[tau-1, self.arms[tau-1]]
+            if not j:
+                self.A_inv = np.linalg.inv(self.A)
+            self.Theta_hat[j] = np.dot(self.A_inv, self.b[j])
+
+        self.t += 1
+        self.Psi = np.append(self.Psi, self.t)
+
+        # self.A[chosen_arm] += np.outer(chosen_arm_features, chosen_arm_features)
+        # self.b[chosen_arm] += reward * chosen_arm_features
--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 from linucb import LinUCB
 from dlinucbd import DLinUCBD
 from alg import Alg
+from bmm import Bmm
 
 
 np.random.seed(123)
 
 
-FEATURE_DIM = 2
-N_ARMS = 2
-N_TRIALS = 6000 # number of total time steps
+FEATURE_DIM = 10
+N_ARMS = 20
+N_TRIALS = 9000 # number of total time steps
 T = N_TRIALS    # number of time steps to be used for learning
 assert T <= N_TRIALS
 
@@ -22,9 +23,15 @@ SIGMA = 1.0  # scale of noise distribution
 STATIONARY = False # setting this to True will make DLinUCB's gamma = 1!!!
 
 
-BABY_EXAMPLE = True  # set to True to reproduce examples from the DLinUCB paper
+BABY_EXAMPLE = False  # set to True to reproduce examples from the DLinUCB paper
 THETA_CHANGE_POINT = 2000
 SLOW_VAR = False  # set to True to simulate slow varying Theta
+
+BMM_TEST = True # generate the same data as in the BMM paper
+BMM_EPS = 1
+BMM_DELTA = 0.01
+BMM_V = 1
+
 
 UNIFORM_FEATURES = True   # generate features using uniform distribution, otherwise use normal distr.
 SCALE_IN_FEATURES = 0.51
@@ -32,16 +39,17 @@ SCALE_IN_THETA = 0.25
 BEST_ARMS = [] # was [3, 7, 9, 15]
 
 
-N_RUNS = 10  # number of realizations
+N_RUNS = 1  # number of realizations
 MEAN = True  # plot mean or median result
 # for plotting error bars
 EVERY_ERR = int(T / 20)
 SHIFT_ERR = int(EVERY_ERR / 10)
 
 # set any number to 0 to not run a given algorithm
-LINUCB = (1 == 1)
-DLINUCB = (1 == 1)
-ALG = (1 == 1)
+LINUCB = (1 == 0)
+DLINUCB = (1 == 0)
+ALG = (1 == 0)
+BMM = (1 == 1)
 
 
 def get_features(n_trials, n_arms, feature_dim, scale):
@@ -52,6 +60,10 @@ def get_features(n_trials, n_arms, feature_dim, scale):
     for t in range(n_trials):
         if UNIFORM_FEATURES:
             features[t] = np.random.uniform(low=0, high=1, size=(n_arms, feature_dim))
+        elif BMM_TEST:
+            for a in range(n_arms):
+                features[t, a] = np.random.uniform(low=0, high=1, size=feature_dim)
+                features[t, a] = features[t, a] / np.linalg.norm(features[t, a])
         else:
             features[t] = np.random.normal(size=(n_arms, feature_dim), scale=scale)
 
@@ -106,6 +118,11 @@ def get_true_theta(n_trials, n_arms, feature_dim, scale, stationary):
                         print("||Theta|| = {} for t = {}".format(np.linalg.norm(theta[t]), t))
                         exit()
             return theta
+    elif BMM_TEST:
+        for t in range(n_trials):
+            for a in range(n_arms):
+                theta[t, a] = np.ones(shape=feature_dim, dtype=np.float64) / np.sqrt(feature_dim)
+        return theta
     
     const_theta = np.random.normal(size=(n_arms, feature_dim), scale=scale)
     new_theta = const_theta
@@ -144,6 +161,8 @@ if DLINUCB:
     regret_d = np.empty((N_RUNS, N_TRIALS))
 if ALG:
     regret_a = np.empty((N_RUNS, N_TRIALS))
+if BMM:
+    regret_b = np.empty((N_RUNS, N_TRIALS))
 
 
 # run N_RUNS realizations
@@ -152,7 +171,10 @@ for run in range(N_RUNS):
     DATA = get_features(N_TRIALS, N_ARMS, FEATURE_DIM, SCALE_IN_FEATURES)
     TRUE_THETA = get_true_theta(N_TRIALS, N_ARMS, FEATURE_DIM, SCALE_IN_THETA, STATIONARY)
     # NOISE = SIGMA * np.random.randn(N_TRIALS)
-    NOISE = np.random.laplace(scale=SIGMA, size=N_TRIALS)
+    if BMM_TEST:
+        NOISE = np.random.standard_t(df=3, size=N_TRIALS)
+    else:
+        NOISE = np.random.laplace(scale=SIGMA, size=N_TRIALS)
     
     EXPECTED_REWARDS = np.array([
             np.max([get_reward(TRUE_THETA[t, arm], DATA[t, arm], NOISE[t]) for arm in range(N_ARMS)]) \
@@ -184,6 +206,17 @@ for run in range(N_RUNS):
         alg = Alg(N_TRIALS, FEATURE_DIM, N_ARMS, DATA[0], TRUE_THETA)
         # ub = np.empty((N_TRIALS, N_ARMS))
         estimated_rewards_a = np.empty(N_TRIALS)
+    
+    if BMM:
+        bmm = Bmm(
+                feature_dim=FEATURE_DIM,
+                n_arms=N_ARMS,
+                eps=BMM_EPS,
+                delta=BMM_DELTA,
+                v=BMM_V,
+                T=N_TRIALS,
+                )
+        estimated_rewards_b = np.empty(N_TRIALS)
 
     # run T time steps
     for t in range(T):
@@ -208,6 +241,15 @@ for run in range(N_RUNS):
             alg.update(chosen_arm, features[chosen_arm], reward, TRUE_THETA) #TODO: change to 3D 
             estimated_rewards_a[t] = reward
     
+        if BMM:
+            chosen_arm = bmm.choose_arm(features)
+            rewards = [get_reward(TRUE_THETA[t, chosen_arm], features[chosen_arm], NOISE[t])]
+            rewards = rewards + [get_reward(TRUE_THETA[t, chosen_arm], features[chosen_arm],
+                np.random.standard_t(df=3)) for j in range(1, bmm.r)]
+            rewards = np.array(rewards)
+            bmm.update(rewards)
+            estimated_rewards_b[t] = np.median(rewards)
+    
     # compute theta error for min-max CMAB algorithm
     # if ALG:
         # theta_err = [np.linalg.norm(alg.Theta[arm] - TRUE_THETA[arm]) /\ 
@@ -220,6 +262,8 @@ for run in range(N_RUNS):
         regret_d[run] = np.cumsum(EXPECTED_REWARDS[:T] - estimated_rewards_d[:T])
     if ALG:
         regret_a[run] = np.cumsum(EXPECTED_REWARDS[:T] - estimated_rewards_a[:T])
+    if BMM:
+        regret_b[run] = np.cumsum(EXPECTED_REWARDS[:T] - estimated_rewards_b[:T])
 
 
 # plot the results
@@ -274,6 +318,22 @@ if ALG:
     plt.errorbar(x=TRIALS[s*SHIFT_ERR:][::EVERY_ERR], y=avg[s*SHIFT_ERR:][::EVERY_ERR],
             yerr=[(avg - low)[s*SHIFT_ERR:][::EVERY_ERR], (up - avg)[s*SHIFT_ERR:][::EVERY_ERR]],
             color='green', linestyle='', capsize=3)
+
+if BMM:
+    s = 3
+    if MEAN:
+        avg = np.mean(regret_b, axis=0)
+        std = np.std(regret_b, axis=0)
+        low = avg - 2.626 * std / np.sqrt(N_RUNS)
+        up = avg + 2.626 * std / np.sqrt(N_RUNS)
+    else:
+        avg = np.median(regret_b, axis=0)
+        low = np.percentile(a=regret_b, q=25, axis=0)
+        up = np.percentile(a=regret_b, q=75, axis=0)
+    plt.plot(avg, label='BMM', color='orange')
+    plt.errorbar(x=TRIALS[s*SHIFT_ERR:][::EVERY_ERR], y=avg[s*SHIFT_ERR:][::EVERY_ERR],
+            yerr=[(avg - low)[s*SHIFT_ERR:][::EVERY_ERR], (up - avg)[s*SHIFT_ERR:][::EVERY_ERR]],
+            color='red', linestyle='', capsize=3)
 
 if BABY_EXAMPLE:
     if SLOW_VAR:
